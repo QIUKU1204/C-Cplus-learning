49c2edaec6ea4da944007af727f6b80bb39141b1
QIUKU1204 <xigua1204012@hotmail.com>
2018-07-23 13:02:58 +0800

    source code scanner

---
+++ source-code-scanner-v1/source-code-scanner-v1/source-code-scanner-v1.cpp
@@ -0,0 +1,36 @@
+// source-code-scanner-v1.cpp : 源代码扫描器
+// TODO:自动扫描.h文件，识别、提取文件中的注释信息生成help文档；
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <regex>
+using namespace std;
+
+/*
+* TODO:判断是否为.h文件，若是则打开，不是则返回提示信息；
+*/
+void isHeadFile() {
+
+}
+
+/*
+* TODO:构建正则表达式提取.h文件中的注释信息，包括单行注释 // 与 段注释 /* ；
+*/
+void extractComments() {
+
+}
+
+/*
+* TODO:利用注释信息生成help文档，可以是Word文档、PDF文件和MD文件的形式；
+*/
+void generateHelpDoc() {
+
+}
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
